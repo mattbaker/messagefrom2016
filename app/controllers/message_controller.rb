03f987ab3ed7508ee243b03337dd481d9537b8fc
Avner Shanan <ashanan@gmail.com>
2016-11-13 20:28:45 -0600

    Add index page and post route

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,13 @@
+class MessageController < ApplicationController
+  def new
+    @message = Message.new
+    
+    @days_to_election = 720
+  end
+
+  def create
+    @message = Message.new(params['message'])
+
+    redirect_to :action => new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'message/new'
+  post 'message/', to: 'message#create', as: 'message_create'
+
+  root 'message#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
