2765bee6e4cc7bdac4870cc1adb4a6bacb45707d
Adam Vera & Corbin Via <pair+ajvera.corbin1987@devbootcamp.com>
2016-11-15 15:18:09 -0600

    finish tests for POST#create

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe MessageController do
-  let!(:message) { Message.create!(content: "bloop", email_address: "me@me.com")}
-  let!(:unsaved_message) {Message.new}
+  let!(:message) { Message.new(content: "bloop", email_address: "me@me.com")}
   describe "GET #new" do
     it "responds with status code 200" do
       get :new
@@ -15,6 +14,30 @@ describe MessageController do
     end
 
   end
+
+  describe "POST #create" do
+
+    it "redirects to root if a message is successfully saved" do
+      post :create, params: { message: {content: "bleep", email_address: "bloop", subject: "AHHHHHHH"} }
+      expect(response).to redirect_to('/')
+    end
+
+    it "renders the 'new' partial if a message is not saved" do
+      post :create, params: {message: {subject: "",content:"",email_address:""} }
+      expect(response).to render_template(:new)
+    end
+
+    describe "validations" do
+
+      it "is invalid when not passed the required strong params" do
+        post :create, params: {message: {subject: "", email_address: "", content: ""}}
+        expect(assigns(:errors)).to_not be_empty
+      end
+
+    end
+  end
+
+
 
 
 
