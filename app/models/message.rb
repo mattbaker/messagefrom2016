4d9593893c5604ff0c43b4f03e9a37576cb6aacb
Avner Shanan <ashanan@gmail.com>
2016-11-14 21:20:19 -0600

    Remove subject input and model validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,5 @@
 class Message < ApplicationRecord
   has_secure_token :auth_token
   validates :content, presence: true
-  validates :subject, presence: true
   validates :email_address, presence: true
 end
