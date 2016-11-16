dda2c0946872ff393aaad836c0f236e5b07c376e
Jsumulong <sumulong28@gmail.com>
2016-11-15 20:07:31 -0600

    add border fade red/fade black feature

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
 			$(this).css("color", "black");
 		} 
 	);
+	$('.button').on('click', function(event) {
+		event.preventDefault();
+		$('#message_email_address').fadeTo( 600, 1, function() {
+			$(this).css("border-color", "red");
+		});
+		$('#message_email_address').delay(600).fadeTo( 600, 1, function() {
+			$(this).css("border-color", "black");
+		});
+	})
+	$('#save').on('click', function(event) {
+		event.preventDefault();
+		$('#message_content').fadeTo( 600, 1, function() {
+			$(this).css("border-color", "red");
+		});
+		$('#message_content').delay(600).fadeTo( 600, 1, function() {
+			$(this).css("border-color", "black");
+		});
+	})
 });
