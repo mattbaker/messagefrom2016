5f6722e62778cfc65be45ec3e4464278020b7963
Matt Baker <mbaker.pdx@gmail.com>
2016-11-15 21:16:02 -0600

    Adjust routes to conform to rails convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get  '/message/new'
-  post '/message/', to: 'message#create', as: 'message_create'
-  get  '/message/confirm/:token', to: 'message#confirm', as: 'message_confirm'
+  get  '/messages/new'
+  post '/messages', to: 'message#create', as: 'message_create'
+  get  '/messages/confirm/:token', to: 'message#confirm', as: 'message_confirm'
   root 'message#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
